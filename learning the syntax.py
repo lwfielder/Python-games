79bdb246bb8373d1ab94cd25914e187a7f96fcb2
Liam Wesley Fielder <lwfielder198@gmail.com>
2018-07-31 22:07:28 -0700

    added celsius to farenheit conversion

--- learning the syntax.py
+++ learning the syntax.py
@@ -1,6 +1,17 @@
+import sys;
 #degrees celsius to farenheit converter
+def celsius_farenheit(cel):
+    f = cel * 1.8 + 32
+    return(f)
 
-value = float(input("what is the temperature in celsius: "))
+def farenheit_celsius(far):
+    c = (far - 32) / 1.8
+    return(c)
 
-f = value * 1.8 + 32
-print(f)
+
+
+decision = raw_input("Would you like to convert from celsius to farenheit or vice versa (enter c or f respectively): ")
+
+if (decision == "c" or decision == "C"):
+    ans = input("what is the temperature in celsius that you would like to know in farenheit: ")
+    print(celsius_farenheit(ans))
